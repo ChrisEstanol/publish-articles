a7737963dc3bc1900e471dc851bf3a4f0d78470f
Christophe E <cestanol@gmail.com>
2014-12-08 16:30:06 -0500

    Add update, edit, new and create js.erb files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  # see note at the bottom
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,10 +11,11 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.new(allowed_params)
+    @post = Post.create(allowed_params)
     # @post.title = params[:post][:title]
     # @post.published = params[:post][:published]
     # @post.author = params[:post][:author]
@@ -20,11 +23,11 @@ class PostsController < ApplicationController
     # This is equivalent to using rails c and entering
     # parameters manualy
 
-    if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
-    else
-      render action: 'new'
-    end
+    # if @post.save
+    #   redirect_to @post, notice: 'Post was successfully created.'
+    # else
+    #   render action: 'new'
+    # end
   end
 
   def show
@@ -32,20 +35,31 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.update(allowed_params)
-      redirect_to @post, notice: 'Post was successfully updated.'
-    else
-      render action: 'edit'
-    end
+    @post = Post.find(params[:id], allowed_params)
+    # if @post.update(allowed_params)
+    #   redirect_to @post, notice: 'Post was successfully updated.'
+    # else
+    #   render action: 'edit'
+    # end
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_url, notice: 'Post was successfully deleted.'
   end
 
+#  only allow the white list through.
   def allowed_params
     params.require(:post).permit([:title, :author, :content, :published])
   end
+
+# we have the option to define a method that use the code we repeat
+# we then declare it with before action
+# private
+#   def set_post
+#     @post = Post.find(params[:id])
+#   end
+
 
 end
